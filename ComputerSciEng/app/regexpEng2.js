05aed2cbda055bccbe7d6d0bb1990e43cd5249d4
Ian Zhang <ianzhang1986@gmail.com>
2015-11-29 02:08:32 -0800

    [Feature] Regexp and Real time embedded system entries

--- ComputerSciEng/app/regexpEng2.js
+++ ComputerSciEng/app/regexpEng2.js
@@ -13,15 +13,20 @@
 
 var symbolTbl = ["^", "*", "\0", ".", "$"];
 
-var match = function(regexp, text) {
-//  if (typeof regexp === 'object') regexp = regexp.toString().split("").slice(1).join("");
-  if (regexp[0] === symbolTbl[0]) 
+var match = function(text, regexp) {
+  var currText = text[0];
+  if (regexp[0] === "^") 
     return matchhere(regexp.slice(1), text);
-  do { /* must look even if string is empty */
+  do {
     if (matchhere(regexp, text))
       return 1;
-  } while (text++ !== symbolTbl[2]);
+  } while (text++ !== "\0");
   return 0;
+};
+
+// Now cook my own version of matchhere 
+var match = function(regexp, text) {
+
 };
 
 /**
@@ -31,7 +36,8 @@ var match = function(regexp, text) {
  * @return {boolean} 
  */
 
-var matchhere = function(regexp, text) {
+/*
+var matchhere = function(text, regexp) {
   if (regexp[0] === symbolTbl[2]) 
     return 1;
   if (regexp[1] === '*') 
@@ -41,7 +47,7 @@ var matchhere = function(regexp, text) {
   if (text!==symbolTbl[2] && (regexp[0] === symbolTbl[3] || regexp[0] === text))
     return matchhere(regexp.slice(1), text.slice(1));
   return 0;
-};
+};*/
 
 /**
  * Match: search for c*regexp at beginning of text
@@ -51,13 +57,14 @@ var matchhere = function(regexp, text) {
  * @return {boolean} 
  */
 
-var matchstar = function(c, regexp, text) {
+/*
+var matchstar = function(c, text, regexp) {
   var currText = "", iter = 0;
   do { // a * matches zero or more instances
     if (matchhere(regexp, text)) return 1;
   } while (text[iter] !== symbolTbl[2] && (text[iter++] === c || c === symbolTbl[3]));
   return 0;
-};
+};*/
 
 
 // TestCase
---
+++ EE/EmbeddedSys/embeddedProg/embeddedProg.md
@@ -0,0 +1,5 @@
+# Real-time programming with BeagleBone PRUs
+
+References
+
+[1] [Real-time programming with BeagleBone PRUs](https://www.youtube.com/watch?v=By9foc1iJ3Q)
