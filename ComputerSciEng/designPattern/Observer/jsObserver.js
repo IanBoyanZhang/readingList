b4b2ee6e5e5638bb272c74765b674b6322815beb
Ian Zhang <ianzhang1986@gmail.com>
2017-01-16 22:35:54 -0800

    [Feature] Observer Pattern test code

--- ComputerSciEng/designPattern/Observer/jsObserver.js
+++ ComputerSciEng/designPattern/Observer/jsObserver.js
@@ -36,3 +36,17 @@ function Subject() {
   this.observers = new ObserverList();
 }
 
+Subject.prototype.addObserver = function(observer) {
+  this.observers.add(observer);
+};
+
+Subject.prototype.removeObserver = function(observer) {
+  this.observers.removeAt( this.observers.indexOf(observer, 0) );
+};
+
+Subject.prototype.notify = function(context){
+  var observerCount = this.observers.count();
+  for (var i = 0; i < observerCount; i+=1) {
+    this.observers.get(i).update(context);
+  }
+};
