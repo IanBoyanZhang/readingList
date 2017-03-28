8da49626e8b9c63a234daac24e4c1262fdce6b9d
Ian Zhang <ianzhang1986@gmail.com>
2017-03-28 13:29:45 -0700

    [Feature] Node.js

--- ComputerSciEng/designPattern/prototypePattern.js
+++ ComputerSciEng/designPattern/prototypePattern.js
@@ -51,3 +51,28 @@ var car = Object.create(vehicle, {
 console.log(MY_GLOBAL.nextId());
 console.log(MY_GLOBAL.nextId());
 console.log(MY_GLOBAL.nextId());
+
+
+var vehiclePrototype = {
+  init: function(carModel) {
+    this.model = carModel;
+  },
+  getModel: function () {
+    console.log("The model of this vehicle is.. " + this.model );
+  }
+};
+
+function Vehicle(model) {
+  function F() {}
+  F.prototype = vehiclePrototype;
+
+  var f = new F();
+
+  f.init( model );
+  return f;
+}
+
+var car = Vehicle("Ford Escort");
+car.getModel();
+
+var beget = (function() {})();
---
+++ WebEng/node.md
@@ -0,0 +1,10 @@
+
+Node.js Ryan Dahl
+
+[Using libuv and http-parser to build a webserver](https://vimeo.com/24713213)
+
+References 
+
+[Understand Node.js event loop](http://stackoverflow.com/questions/10680601/nodejs-event-loop)
+
+[Libuv book](http://luohaha.github.io/Chinese-uvbook/index.html)
